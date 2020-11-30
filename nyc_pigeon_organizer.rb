56fc01efc8e171c1df6cb0033d39a7a728dd8808
erin dougher <ejdougher@gmail.com>
2020-11-30 08:29:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -21,7 +21,7 @@ pigeon_data = {
 
 def nyc_pigeon_organizer(data)
   # write your code here!
-  final_results = data.each_with({}) do |(key, value), final_array|
+  final_results = data.each_with_object({}) do |(key, value), final_array|
 
     value.each do |inner_key, names|
       names.each do |name|
